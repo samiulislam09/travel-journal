33ab997428e6a57dbb883264aa5153ea013cf40d
samiulislam09 <samiulislamsawon09@gmail.com>
2024-11-17 16:04:51 +0600

    completed design of sign in page

--- lib/screens/signin/signin.dart
+++ lib/screens/signin/signin.dart
@@ -8,29 +8,81 @@ class SignIn extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Sign In'),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        title: const Text(
+          'Sign In',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
       ),
       body: Center(
           child: Column(
         children: [
-          TextField(
-            decoration: const InputDecoration(
-              labelText: 'Email',
-            ),
+          SizedBox(height: 20),
+          Container(
+            height: 300,
+            width: double.infinity,
+            child: Image.asset('assets/images/Riding.png'),
           ),
-          TextField(
-            decoration: const InputDecoration(
-              labelText: 'Password',
+          SizedBox(height: 30),
+          Padding(
+            padding: const EdgeInsets.all(12),
+            child: Column(
+              children: [
+                TextField(
+                  decoration: const InputDecoration(
+                      labelText: 'Email', border: OutlineInputBorder()),
+                ),
+                SizedBox(height: 20),
+                TextField(
+                  decoration: const InputDecoration(
+                      labelText: 'Password', border: OutlineInputBorder()),
+                ),
+                SizedBox(height: 20),
+                Container(
+                  width: double.infinity,
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => HomeScreen()),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Theme.of(context).colorScheme.primary,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    child: const Text(
+                      'Sign In',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text('Don\'t have an account?'),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        'Sign Up',
+                        style: TextStyle(
+                            color: Colors.blue,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const HomeScreen()),
-              );
-            },
-            child: const Text('Sign In'),
           ),
         ],
       )),
